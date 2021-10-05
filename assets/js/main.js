f60ff955e6e6754e26e564a7210c3f05098b765f
Eugene <zkerov9@gmail.com>
2021-10-05 18:00:35 +0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+let name = '';
+
 let game = {
     game: []
 };
@@ -7,6 +9,7 @@ let panel = 'start';
 let nav = () => {
     document.onclick = (e) => {
         e.preventDefault();
+
         switch (e.path[0].id) {
             case 'startGame':
                 go('game', 'd-block');
@@ -14,27 +17,47 @@ let nav = () => {
             case 'restart':
                 go('game', 'd-block');
                 $('.elements').remove();
-                $('#game').append('<div class="elements"></div>')
+                $('#game').append(`<div class="elements"></div>`);
                 break;
         }
-    }
+    };
 };
 
 let go = (page, attribute) => {
     let pages = ['start', 'game', 'end'];
+
     panel = page;
     $(`#${page}`).attr('class', attribute);
     pages.forEach(e => {
         if (page != e) {
             $(`#${e}`).attr('class', 'd-none');
         }
-    })
-}
+    });
+};
 
-let start = () => {
+let startloop = () => {
     let inter = setInterval(() => {
         if (panel !== 'start') {
             clearInterval(inter);
         }
-    }, 100)
-}
+        checkName();
+    }, 100);
+};
+
+let checkStorage = () => {
+    if (localStorage.getItem('userName') != null) {
+        $(`#nameInput`).val(localStorage.getItem('userName'));
+    }
+};
+
+let checkName = () => {
+    name = $(`#nameInput`).val().trim();
+
+    if (name != '') {
+        localStorage.setItem('userName', name);
+        $('#startGame').attr('disabled', false);
+    }
+    else {
+        $('#startGame').attr('disabled', true);
+    }
+};
